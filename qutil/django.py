debeb51074c3806851388eae415103cab93624e1
jason.chen <jc.news@outlook.com>
2018-07-25 13:17:51 -0400

    fixed model type

--- qutil/django.py
+++ qutil/django.py
@@ -5,7 +5,7 @@ def df_to_django_model(df):
         elif type_ == 'object':
             dj_type = 'models.CharField(max_length=200, null=True, blank=False)'
         elif type_ == 'float':
-            dj_type = 'models.FloatField(max_length=200, null=True, blank=False)'
+            dj_type = 'models.FloatField(null=True, blank=False)'
         elif type_ == 'datetime64[ns]':
             dj_type = 'models.DateTimeField(null=True, blank=False)'
         else:
